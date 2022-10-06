9a37d1d11f6c78b5db574464396cee7d6b170ffd
Aldik <andi.kitta77@gmail.com>
2022-10-06 22:35:55 +0800

    AppBar

---
+++ app/src/main/java/com/aldikitta/adaptivelayoutjetpackcompose/presentation/components/ReplyProfileImage.kt
@@ -0,0 +1,26 @@
+package com.aldikitta.adaptivelayoutjetpackcompose.presentation.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ReplyProfileImage(
+    drawableResource: Int,
+    description: String,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        modifier = modifier.size(40.dp).clip(CircleShape),
+        painter = painterResource(id = drawableResource),
+        contentDescription = description,
+    )
+}
+
